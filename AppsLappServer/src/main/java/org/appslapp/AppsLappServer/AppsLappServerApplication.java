06ebcdcee099071f88cf3dac007a47950f4bf7a8
Jakub Kapitulcin <jakkappro@gmail.com>
2022-02-03 17:59:49 +0100

    first commit

---
+++ AppsLappServer/src/main/java/org/appslapp/AppsLappServer/AppsLappServerApplication.java
@@ -0,0 +1,13 @@
+package org.appslapp.AppsLappServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsLappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsLappServerApplication.class, args);
+	}
+
+}
---
+++ AppsLappServer/src/test/java/org/appslapp/AppsLappServer/AppsLappServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appslapp.AppsLappServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsLappServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
