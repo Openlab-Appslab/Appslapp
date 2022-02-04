e6874172c75e4b802472a29c61a68c81ca806e92
Jakub Kapitulcin <jakkappro@gmail.com>
2022-02-04 12:56:51 +0100

    separate server and web

--- AppsLappServer/src/main/java/org/appslapp/AppsLappServer/persistance/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.appslapp.AppsLappServer.persistance;
-
-import org.appslapp.AppsLappServer.business.user.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
