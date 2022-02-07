cda013c0faffd523ea7c875823999dd3b0050194
FilipDavid1 <filipenkodavid@gmail.com>
2022-02-07 19:11:54 +0100

    add component for dashboard

--- AppsLapp-web/src/app/app-routing.module.ts
+++ AppsLapp-web/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './User-operations/login/login.component';
 import { RegistrationComponent } from './User-operations/registration/registration.component';
+
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/registration', pathMatch: 'full'},
   {path: 'registration', component: RegistrationComponent },
-  {path: 'login', component: LoginComponent }
+  {path: 'login', component: LoginComponent },
+  {path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
